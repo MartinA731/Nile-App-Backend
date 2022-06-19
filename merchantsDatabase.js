b1899f85e5aa3d74f646c95d40cd5d1c8889d8a4
MartinA731 <mackerma@andrew.cmu.edu>
2022-06-19 08:03:43 -0600

    added clients

--- merchantsDatabase.js
+++ merchantsDatabase.js
@@ -4,7 +4,12 @@
 
 let merchants = [
     { email: "user1@gmail.com", clients: []},
-    { email: "user2@gmail.com", clients: []},
+    { email: "user2@gmail.com", 
+      clients: [{name : "John Smith", pickupID : "RA38990125US", date : "3/9/2022"},
+                {name : "Tom Silver", pickupID : "RA38990126US", date : "3/11/2022"},
+                {name : "LeBron James", pickupID : "RA38990127US", date : "4/20/2022"},
+                {name : "Andrew Carnegie", pickupID : "RA38990128US", date : "5/11/2022"},
+                {name : "Joe Biden", pickupID : "RA38990129US", date : "6/1/2022"}]},
     { email: "user3@gmail.com", clients: []}
   ];
 
