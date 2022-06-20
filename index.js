ebabb1767841e8e3c85522c6bc97793d50c84a86
MartinA731 <mackerma@andrew.cmu.edu>
2022-06-19 18:06:35 -0600

    factorization

--- index.js
+++ index.js
@@ -4,16 +4,12 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const port = process.env.PORT || 3001;
-const usersRouter = require("./routes/users");
-//addtion
 const merchantRouter = require("./routes/merchants")
 
 app.use(logger("dev"));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use("/users", usersRouter);
-//addition
 app.use("/merchants", merchantRouter);
 app.listen(port, function() {
   console.log("Runnning on " + port);
